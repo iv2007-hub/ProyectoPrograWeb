b3d2ace7a739c4471b027d5630f96888a2fa1e90
iv2007-hub <ivanna_trujillo@unitec.edu>
2026-05-31 09:53:30 -0600

    nuevos cambios

---
+++ ProyectoPrograWeb/models/experiment.cs
@@ -0,0 +1,20 @@
+﻿namespace ProyectoPrograWeb.models;
+
+public class experiment
+{
+    public string Id { get; set; } = string.Empty;
+    
+    // Titulo o nombre al experimento
+    public string Title { get; set; } = string.Empty;
+    
+    // Resultado o resumen del experimento
+    public string Result { get; set; } = string.Empty;
+    
+    // Guardamos el id del usuario que creo el experimento o prueba
+    public string UserId { get; set; } = string.Empty;
+    
+    // Resultado, funciono o no
+    public bool Success { get; set; } = false;
+    
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
